0d0346316c42d7ade809f85fa0969a287ce607e3
johnydotsville <come.as@you.are>
2022-05-06 12:07:13 +0300

    Сделал черновик получения url из файла

--- src/main/java/johny/dotsville/App.java
+++ src/main/java/johny/dotsville/App.java
@@ -2,11 +2,15 @@ package johny.dotsville;
 
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.concurrent.Callable;
+import java.util.stream.Stream;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
@@ -17,14 +21,15 @@ import johny.dotsville.utils.*;
 @Command(name = "SomeRandomName")
 public class App implements Callable<Integer>
 {
-    @Option(names = { "-u", "--url" }, arity = "1..*", description = "Url источник информации")
-    private String[] urls;
+    // arity - чтобы в опцию можно было передавать разом много значений, -u a1 b2 c3 d4
+//    @Option(names = { "-u", "--url" }, arity = "1..*", description = "Url источник информации")
+//    private String[] urls;
 
     @Option(names = { "-o", "--output" }, description = "Файл для сохранения результата")
     private String outputFile;
 
-//    @Option(names = { "-ul", "--urllist" }, description = "File with urls download content from")
-//    private String urlList;
+    @Option(names = { "-ul", "--urllist" }, description = "File with urls download content from")
+    private String urlList;
 
     public static void main(String[] args)
     {
@@ -34,12 +39,17 @@ public class App implements Callable<Integer>
 
     @Override
     public Integer call() throws Exception {
-        process(urls);
+//        process(urls);
+        process();
         return 0;
     }
 
-    private void process(String[] rawUrls) {
-        List<URL> urls = Arrays.stream(rawUrls)
+//    private void process(String[] rawUrls) {
+//    private void process(String rawUrlsList) {
+    private void process() {
+
+//        List<URL> urls = Arrays.stream(rawUrls)
+        List<URL> urls = getRawUrlsFromFile(urlList)
                 .map(l -> UrlHelper.urlFromString(l))
                 .filter(e -> e.isRight())
                 .map(e -> e.getRight())
@@ -70,6 +80,15 @@ public class App implements Callable<Integer>
             FileWriter.write(names, outputFile);
         } catch (IOException ex) {
 
+        }
+    }
+
+    private Stream<String> getRawUrlsFromFile(String file) {
+        Path path = Paths.get(file);
+        try {
+            return Files.lines(path);
+        } catch (IOException ex) {
+            throw new IllegalArgumentException("Файл " + file + " не существует или его невозможно прочитать.");
         }
     }
 }
